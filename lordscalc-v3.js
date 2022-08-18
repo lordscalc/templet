ed229b0585e4806562686f2288d0352b33ddb4bf
lordscalc <lm@lordscalc.com>
2022-08-18 21:05:50 +0200

    Adding css and js file

---
+++ lordscalc-v3.js
@@ -0,0 +1,13 @@
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'interactive') {
+        const instant = new lordscalc();
+    }
+});
+
+
+class lordscalc {
+
+    constructor() {
+    }
+
+}
